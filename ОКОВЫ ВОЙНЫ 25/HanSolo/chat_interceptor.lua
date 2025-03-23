44a01c1eff395b50d339643ccbacb540ad4772b0
Andrey Iskrich <aviskriche@gmail.com>
2025-03-23 15:59:39 +0400

    Корректная обработка перехвата чата Ставим метки по ключу :mark Пример: :mark N24 53.343 E23 23.523 Message

--- ОКОВЫ ВОЙНЫ 25/HanSolo/chat_interceptor.lua
+++ ОКОВЫ ВОЙНЫ 25/HanSolo/chat_interceptor.lua
@@ -1,17 +1,5 @@
 ---dcshook
 
-local Inspect = require("inspect")
-local lfs = require("lfs")
-
-local function dumpObject(obj, filename)
-  local path = lfs.writedir()
-  local file = io.open(path .. filename .. ".lua", "w")
-  if file then
-    file:write(filename .. " = " .. Inspect(obj))
-    file:close()
-  end
-end
-
 ---@class ChatInterceptor : EventHandler
 local ChatInterceptor = {}
 function ChatInterceptor.onPlayerTryChangeSlot(playerid, side, slotId)
@@ -22,15 +10,47 @@ function ChatInterceptor.onPlayerTryChangeSlot(playerid, side, slotId)
   return true
 end
 
-function ChatInterceptor.onPlayerTrySendChat(playerId, msg, all)
-  local playerName = net.get_player_info(playerId).name
+local counter = 150000  
 
-  if ChatProcessor ~= nil then
-    net.log("chat hook")
-    net.dostring_in('server', string.format("ChatProcessor.process(%s, %s)", playerName, msg))
+function ChatInterceptor.onPlayerTrySendChat(playerId, msg, all)
+  net.log("chat hook")
+  -- net.dostring_in('server', string.format("ChatProcessor.process(%s, %s)", playerName, msg))  
+
+  local pattern = "^:([%w_]+)%s+(.*)"
+  local keyword, data = string.match(msg, pattern)
+  net.log(string.format("CHAT_PROCESSOR: %s; %s", keyword, data))
+
+  if keyword then
+      -- Нашли совпадение
+      if keyword == "mark" then
+          local coalition = net.get_player_info(playerId).side
+
+          -- Обрабатываем команду set_mark
+          local latDir = "([NSns])"
+          local longDir = "([EWew])"
+          local ddmLat = "(%d%d?)[^%d]+(%d%d?)[^%d]+(%d%d%d?%d?)"
+          local ddmLong = "(%d%d?%d?)[^%d]+(%d%d?)[^%d]+(%d%d%d?%d?)"
+          local strDat = "%s*(.*)"
+
+          local regexp = string.format(".*%s.-%s.*%s.-%s.-%s", latDir, ddmLat, longDir, ddmLong, strDat)
+          net.log(string.format("CHAT_PROCESSOR: regexp: %s", regexp))
+
+          local latN, latD, latM, latDM, longE, longD, longM, longDM, str = data:match(regexp)
+		      net.log(string.format("CHAT PROCESSOR: latN=%s, latD=%s, latM=%s, latDM=%s, longE=%s, longD=%s, longM=%s, longDM=%s, str=%s",latN, latD, latM, latDM, longE, longD, longM, longDM, str))
+
+          local lat = tonumber(latD) + tonumber(latM..'.'..latDM)/60
+          local long = tonumber(longD) + tonumber(longM..'.'..longDM)/60
+          net.log(string.format("CHAT_PROCESSOR: lat: %s, long: %s", lat, long))
+          
+          local command = string.format("trigger.action.markToCoalition(%s, '%s', coord.LLtoLO(%s, %s), %s, false, '%s')", counter, str, lat, long, coalition, str)
+          net.log(command)
+    
+          net.dostring_in('server', command)
+          counter=counter+1
+
+          return true
+      end
   end
 end
-
-dumpObject(DCS, "DCS")
 
 DCS.setUserCallbacks(ChatInterceptor)
